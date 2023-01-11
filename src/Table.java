dacaa2e16de38dc481c2f48f9fed98ba13a7f3cd
Reinbeers <fastmaniac3@gmail.com>
2023-01-11 16:28:30 +0000

    Classes adicionadas

--- src/Table.java
+++ src/Table.java
@@ -1,2 +1,37 @@
 public class Table {
+    boolean free;
+    String order;
+
+
+    public void Table(boolean free, String order) {
+        this.free = free;
+        this.order = order;
+
+
+
+        public void occupy() {
+            free = false;
+        }
+        public void unoccupy() {
+            free = true;
+        }
+
+
+        public void getOrder {
+        }
+        public void saveOrder {
+        }
+        public void cleanOrder {
+        }
+
+        public void isFree() {
+            return boolean free;
+        }
+
+
+
+
+
+
+    }
 }
