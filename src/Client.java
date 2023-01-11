ca93e285bb82da941565afc790da13bd1642faa3
jp <joao.rodrigues@mindswap.academy>
2023-01-11 16:27:10 +0000

    create constructed method Client

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,18 @@
-public class Client {
+public class Client{
+    int tableNumber=4;
+    private String name;
 
+    public Client (String name){
+        this.name = name;
+    }
+public void askForTable(){
 
+        }
+public void order(){
 
+        }
+public void pay(){
 
+        }
 
-    public void askForTable(){}
-    public void order(){}
-    public void pay(){}
-    public void setRestaurant(){}
-}
+        }
