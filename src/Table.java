46048fdd93b0e9686575da4ed713f63726207427
Reinbeers <fastmaniac3@gmail.com>
2023-01-11 17:28:14 +0000

    removed errors in Table

--- src/Table.java
+++ src/Table.java
@@ -1,12 +1,20 @@
 public class Table {
     boolean free;
     String order;
-    String orderName;
 
+
+    public String getOrder() {
+        return order;
+    }
+    public boolean getFree(){
+        return free;
+    }
 
     public Table(boolean free, String order) {
         this.free = free;
         this.order = order;
+
+
     }
 
 
@@ -18,8 +26,7 @@ public class Table {
     }
 
 
-    public void getOrder() {
-    }
+
     public void saveOrder() {
     }
     public void cleanOrder() {
